06132e0b5f933b4d3c861568ca91c11d2cf9198b
unknown <Administrador@OEI-103.hndm.local>
2025-02-20 18:12:46 -0500

    20/02/2025 18.12

---
+++ public/build/assets/formatters-CgleP3Gf.js
@@ -0,0 +1,1 @@
+/* empty css              */import{c as t}from"./VAvatar-C88WUKqv.js";const n=t("v-spacer","div","VSpacer"),o=r=>r?r.split(" ").map(a=>a.charAt(0).toUpperCase()).join(""):"";export{n as V,o as a};
---
+++ public/build/assets/useAbility-B8w35vNn.js
@@ -0,0 +1,1 @@
+import{bw as i}from"./app-B3I6Gn6w.js";const t=()=>i();export{t as u};
